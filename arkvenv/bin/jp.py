d0621d7ecd1bfec132346e1c1e7cd02a0503c034
Adrian Guzman <afg30@case.edu>
2020-12-08 21:39:39 -0600

    Reorganized venv

---
+++ arkvenv/bin/jp.py
@@ -0,0 +1,7 @@
+#!/Users/Guzartz/Code/python_projects/ARK_Analyzer/arkvenv/bin/python
+# EASY-INSTALL-DEV-SCRIPT: 'jmespath==0.10.0','jp.py'
+__requires__ = 'jmespath==0.10.0'
+__import__('pkg_resources').require('jmespath==0.10.0')
+__file__ = '/Users/Guzartz/Code/python_projects/ARK_Analyzer/arkvenv/src/jmespath/bin/jp.py'
+with open(__file__) as f:
+    exec(compile(f.read(), __file__, 'exec'))
